2c3e9393355e1179c7af14dccfce7e264681e02c
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2025-05-14 13:33:01 -0400

    fix: use right version of optimizer

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -4,6 +4,6 @@ echo "Optimizing contracts..."
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/optimizer:0.16.0
+  cosmwasm/optimizer-arm64:0.16.1
 
 echo "Optimization complete!"
